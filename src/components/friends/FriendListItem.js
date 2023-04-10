f565ceb8fb25a84c64a8abf1c10107412bc5bb91
Maria Novosad <100295231+avemasha@users.noreply.github.com>
2023-04-10 21:22:23 +0200

    styles

--- src/components/friends/FriendListItem.js
+++ src/components/friends/FriendListItem.js
@@ -2,20 +2,20 @@ import PropTypes from 'prop-types';
 import css from './Friends.module.css'
 
 const FriendsListItem = ({avatar, name, isOnline, id}) => {
-    const _getStyles = (isOnline) => {
-        if (isOnline){
-            return {
-                backgroundColor: 'green'
-            } }
-      return {
-        backgroundColor: 'red'
-      }
-       }
+    // const _getStyles = (isOnline) => {
+    //     if (isOnline){
+    //         return {
+    //             backgroundColor: 'green'
+    //         } }
+    //   return {
+    //     backgroundColor: 'red'
+    //   }
+    //    }
 
        return (
         
         <li className={css.item} key = {id}>
-       <span className={css.status} style={_getStyles(isOnline)}></span>
+       <span className={css.status} style={{backgroundColor: isOnline ? 'green' : 'red' }}></span>
         <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
         <p className={css.name}>{name}</p>
       </li>
--- src/components/statistics/statistics.js
+++ src/components/statistics/statistics.js
@@ -5,7 +5,7 @@ import { getRandomColor } from 'components/randomColor';
 const Statistics = ({title, stats}) =>{
     return (
         <section className={css.statistics}>
-      {title && <h2 className={css.title}>Upload stats</h2>}
+      {title && <h2 className={css.title}>{title}</h2>}
   
 
   <ul className={css.stat_list}>
